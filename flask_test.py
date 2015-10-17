0ab9192dbc9376c64dc5589c2457e6f9c7b4ab8c
Taras <taras.kazydub@pb.com>
2015-10-17 14:27:31 +0300

    adding timer controls

--- flask_test.py
+++ flask_test.py
@@ -1,9 +1,11 @@
-from flask import Flask,jsonify, render_template, request
+from flask import Flask,jsonify, render_template, request, send_from_directory
 from support.get_jenkins_jobs import JenkinsFeatures
 from support.get_calendar_events import CalendarEvents
 from support.database import WriteToDb
-app = Flask(__name__)
+from flask.ext.triangle import Triangle
 
+app = Flask(__name__)
+Triangle(app)
 
 DATABASE = 'support/test.db'
 db = WriteToDb(DATABASE)
@@ -27,6 +29,10 @@ def get_events():
 def jenkins():
     return JenkinsFeatures().get_jenkins_log()
 
+@app.route('/index/<path:path>')
+def send_js(path):
+    print "path: " + str(path)
+    return send_from_directory("templates",path)
 
 # @app.route('/events')
 # def events():
