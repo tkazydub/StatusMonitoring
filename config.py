0fe9b3397d57ced43b0b8bfd79ba2ba49fe85895
alekseyp <alekseyp@cogniance.com>
2015-10-26 09:27:54 +0200

    Added API to retirieve list of jobs

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ __author__ = 'apopov'
 host = 'http://ci-netpulse.cogniance.com/'
 username = 'kid911'
 password = 'f0da03e34dec1f4d323ae83e9fe45ced'
+jobs_list = ["DEV Galaxy Server Build", "Netpulse-Android"]
--- support/get_jenkins_jobs.py
+++ support/get_jenkins_jobs.py
@@ -9,15 +9,25 @@ class JenkinsFeatures():
     username = config.username
     password = config.password
     server = jenkins.Jenkins(host, username=username, password=password)
+    jobs_list = config.jobs_list
 
     def get_jenkins_log(self):
         J = jenkins.Jenkins('http://erebor.ct.pb.com/')
         return J['test_saas-shipping-ui_custom_suite'].get_last_good_build().get_console()
 
     def get_last_build_info(self, name):
-        job_info = self.server.get_job_info(name)
-        last_job_info = self.server.get_build_info(name, job_info['lastCompletedBuild']['number'])
-        last_build_info = json.dumps({"fullDisplayName": last_job_info['fullDisplayName'],
-                                      "status": last_job_info['result'],
-                                      "building": last_job_info['building']})
-        return last_build_info
+        response = []
+
+        for job in self.jobs_list:
+            job_info = self.server.get_job_info(job)
+            last_build_info = self.server.get_build_info(job, job_info['lastCompletedBuild']['number'])
+
+            if last_build_info['result'] == 'SUCCESS' or last_build_info['result'] == 'ABORTED':
+                response.append(dict(fullName=last_build_info['fullDisplayName'],
+                                      status=last_build_info['result']))
+
+            elif last_build_info['result'] == 'FAILURE':
+                response.append(dict(fullName=last_build_info['fullDisplayName'],
+                                      status=last_build_info['result'],
+                                      startedBy=last_build_info['actions'][1]['causes'][0]['userName']))
+        return response
